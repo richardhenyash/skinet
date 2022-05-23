ffcd45b687e39408625fd60f1ec108af1a618ed8
richardhenyash <richardash1@gmail.com>
2022-05-23 16:56:40 +0100

    End Of Section 5 Part 55

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -3,15 +3,11 @@ using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
 using Core.Specifications;
-using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class ProductsController : ControllerBase
+    public class ProductsController : BaseApiController
     {
         private readonly IGenericRepository<Product> _productsRepo;
         private readonly IGenericRepository<ProductBrand> _productBrandRepo;
