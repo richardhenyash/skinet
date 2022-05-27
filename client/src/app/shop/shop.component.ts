50b36dc57fbfa8e94161357b9dc93d102ee604ff
richardhenyash <richardash1@gmail.com>
2022-05-27 17:54:11 +0100

    Section 9 part 103 complete

--- client/src/app/shop/shop.component.ts
+++ client/src/app/shop/shop.component.ts
@@ -41,28 +41,34 @@ export class ShopComponent implements OnInit {
       this.totalCount = response.count;
     }, error => {
       console.log(error);
-    })
+    });
   }
+
   getBrands() {
     this.shopService.getBrands().subscribe(response => {
-      this.brands = [{id: 0, name: 'All'}, ...response];
+      this.brands = [{ id: 0, name: 'All' }, ...response];
     }, error => {
       console.log(error);
-    })
+    });
   }
+
   getTypes() {
     this.shopService.getTypes().subscribe(response => {
-      this.types = [{id: 0, name: 'All'}, ...response];
+      this.types = [{ id: 0, name: 'All' }, ...response];
     }, error => {
       console.log(error);
-    })
+    });
   }
+
   onBrandSelected(brandId: number) {
     this.shopParams.brandId = brandId;
+    this.shopParams.pageNumber = 1;
     this.getProducts();
   }
+
   onTypeSelected(typeId: number) {
     this.shopParams.typeId = typeId;
+    this.shopParams.pageNumber = 1;
     this.getProducts();
   }
 
@@ -72,7 +78,9 @@ export class ShopComponent implements OnInit {
   }
 
   onPageChanged(event: any) {
-    this.shopParams.pageNumber = event.page;
-    this.getProducts();
+    if (this.shopParams.pageNumber !== event) {
+      this.shopParams.pageNumber = event;
+      this.getProducts();
+    }
   }
 }
