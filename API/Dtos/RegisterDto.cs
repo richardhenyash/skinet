8eb1e91d883fded4da261ff87a7ff9e0076c2ecb
Richard Ash <richard.ash@solirius.com>
2022-12-02 09:35:33 +0000

    End of part 185

--- API/Dtos/RegisterDto.cs
+++ API/Dtos/RegisterDto.cs
@@ -1,10 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace API.Dtos;
 
 public class RegisterDto
 {
+    [Required]
     public string DisplayName { get; set; }
     
+    [Required]
+    [EmailAddress]
     public string Email { get; set; }
     
+    [Required]
+    [RegularExpression("(?=^.{6,10}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\\s).*$",
+        ErrorMessage = "Password must have 1 uppercase character, 1 lowercase character, 1 numeric character, 1 non alphanumeric character and at least 6 characters")]
     public string Password { get; set; }
 }
