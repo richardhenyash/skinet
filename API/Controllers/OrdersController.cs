822fa9848d589892c2901d280306184263901d2f
Richard Ash <richard.ash@solirius.com>
2023-01-19 09:29:28 +0000

    End of part 226

--- API/Controllers/OrdersController.cs
+++ API/Controllers/OrdersController.cs
@@ -33,19 +33,19 @@ public class OrdersController : BaseApiController
     }
 
     [HttpGet]
-    public async Task<ActionResult<IReadOnlyList<Order>>> GetOrderForUser()
+    public async Task<ActionResult<IReadOnlyList<OrderDto>>> GetOrdersForUser()
     {
         var email = HttpContext.User.RetrieveEmailFromPrinciple();
         var orders = await _orderService.GetOrdersForUserAsync(email);
-        return Ok(orders);
+        return Ok(_mapper.Map<IReadOnlyList<Order>, IReadOnlyList<OrderToReturnDto>>(orders));
     }
     [HttpGet("{id}")]
-    public async Task<ActionResult<Order>> GetOrderByIdForUser(int id)
+    public async Task<ActionResult<OrderToReturnDto>> GetOrderByIdForUser(int id)
     {
         var email = HttpContext.User.RetrieveEmailFromPrinciple();
         var order = await _orderService.GetOrderByIdAsync(id, email);
         if (order == null) return NotFound(new ApiResponse(404));
-        return order;
+        return _mapper.Map<Order, OrderToReturnDto>(order);
     }
 
     [HttpGet("deliveryMethods")]
--- API/Helpers/MappingProfiles.cs
+++ API/Helpers/MappingProfiles.cs
@@ -1,7 +1,7 @@
 using API.Dtos;
 using AutoMapper;
 using Core.Entities;
-using Core.Entities.Identity;
+using Core.Entities.OrderAggregate;
 
 namespace API.Helpers
 {
@@ -13,10 +13,12 @@ namespace API.Helpers
                 .ForMember(d => d.ProductBrand, o => o.MapFrom(s => s.ProductBrand.Name))
                 .ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType.Name))
                 .ForMember(d => d.PictureUrl, o => o.MapFrom<ProductUrlResolver>());
-            CreateMap<Address, AddressDto>().ReverseMap();
+            CreateMap<Core.Entities.Identity.Address, AddressDto>().ReverseMap();
             CreateMap<CustomerBasketDto, CustomerBasket>();
             CreateMap<BasketItemDto, BasketItem>();
             CreateMap<AddressDto, Core.Entities.OrderAggregate.Address>();
+            CreateMap<Order, OrderToReturnDto>();
+            CreateMap<OrderItem, OrderItemDto>();
         }
     }
 }
